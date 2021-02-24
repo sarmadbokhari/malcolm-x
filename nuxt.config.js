5da40995439c970f9bf7f4a1042e6fa7b1941808
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2021-02-23 21:17:46 -0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'malcolm-x',
+    title: 'Malcolm X Events, Videos, & Quotes',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
